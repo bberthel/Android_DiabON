62b4151bec4b2878892fb5382a1bdcd2126acf95
Florent <enius76@hotmail.com>
2015-03-26 00:17:17 +0100

    pas de consooole

--- app/src/main/java/project/android/imac/diabon/CategoryActivity.java
+++ app/src/main/java/project/android/imac/diabon/CategoryActivity.java
@@ -1,6 +1,9 @@
 package project.android.imac.diabon;
 
 import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -15,54 +18,18 @@ import android.widget.TextView;
  * Created by Brice on 13/03/2015.
  */
 public class CategoryActivity extends NavigationDrawer {
-
-    ListView listview;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.category);
 
-        listview = (ListView)findViewById(R.id.listView);
-        // Array holding our data
-        String[] category_list = {
-                "Pains, crackers, céréales, pâtisseries",
-                "Féculents et légumes secs",
-                "Légumes",
-                "Fruits et fruits secs",
-                "Produits laitiers",
-                "Plats cuisinés",
-                "Desserts",
-                "Produits sucrés, confiseries",
-                "Appéritifs",
-                "Boissons",
-                "Restauration rapide",
-                "Sauces, assaisonnements"
-        };
-        //adapter which will convert each data item into view item.
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.category_row_view, R.id.listText, category_list);
-        //place each view-item inside listview by setting adapter for our listview
-        listview.setAdapter(adapter);
-
-        listview.setOnItemClickListener(new ListClickHandler());
-    }
-
-    public class ListClickHandler implements AdapterView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
-            // TODO Auto-generated method stub
-            TextView listText = (TextView) view.findViewById(R.id.listText);
-            String text = listText.getText().toString();
-
-            // create intent to start another activity
-            Intent intent = new Intent(CategoryActivity.this, CategoryDetailsActivity.class);
-            // add the selected text item to our intent.
-            intent.putExtra("selected-item", text);
-            startActivity(intent);
-
+        if ( savedInstanceState == null ) {
+             String id = getIntent().getStringExtra("id");
+            //create main fragment : homepage of an event
+            Fragment fragment = new CategoryFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame,fragment).commit();
         }
-
     }
 
 }
